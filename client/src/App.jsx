6a27fd1334fb221f223efa11e81b15ea9f9a2b59
pat <patricklouis.rivera.cics@ust.edu.ph>
2025-08-05 20:42:02 +0800

    remove user route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Books from './pages/Books'
 import Create from './pages/Create'
 import Update from './pages/Update'
-import User from './pages/User'
 import Signup from './pages/auth/Signup'
 import Login from './pages/auth/Login'
 
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<Books/>} />
         <Route path="/create" element={<Create/>} />
         <Route path="/update/:id" element={<Update/>} />
-        <Route path="/user" element={<User/>} />
         <Route path="/signup" element={<Signup/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
